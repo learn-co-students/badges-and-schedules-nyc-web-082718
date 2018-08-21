266ec7a1f0294766257ee28e66a80f2075ac1f89
srscalia <shelby.scalia@gmail.com>
2018-08-21 11:18:25 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  for name in array
+    new_array.push(badge_maker(name))
+  end
+  new_array
+end
+
+def assign_rooms(speakers)
+  assignment = []
+  index = 0
+  for speaker in speakers
+    assignment.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
+    index+=1
+  end
+  assignment
+end
+
+def printer(attendees)
+  loop1 = batch_badge_creator(attendees)
+  for badge in loop1
+    puts badge
+  end
+  loop2 = assign_rooms(attendees)
+  for item in loop2
+    puts item
+  end
+end
